ff4c6b17c5a4cdddf5f0b780e6099b9879f6720c
Paola <pa_rosa@msn.com>
2023-03-27 18:07:53 -0300

    feat: tarefa 0 finalizada

---
+++ exceptions.py
@@ -0,0 +1,14 @@
+class NegativeTitlesError(Exception):
+    def __init__(self, message):
+        self.message = message
+
+
+class InvalidYearCupError(Exception):
+    def __init__(self, message):
+        self.message = message
+        # raise "there was no world cup this year"
+
+
+class ImpossibleTitlesError(Exception):
+    def __init__(self, message):
+        self.message = message
